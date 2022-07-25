b7eead3fc6b3c4a42a502d7d4349f35365af7206
fish-paulo <fish.paulo@gmail.com>
2022-07-25 20:46:05 -0300

    add classes conta
    Criação das classes conta, conta poupança e conta corrente.

---
+++ src/Conta.java
@@ -0,0 +1,34 @@
+public class Conta {
+
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+ 
+    }
+
+    public void transferir() {
+
+    }
+
+    public int getAgencia(){
+        return agencia;
+    }
+
+    
+    public int getNumero(){
+        return numero;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+    
+    
+    
+}
